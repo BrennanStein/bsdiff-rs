3aefdf21196b88e23447a01f3a8eed064707fc7e
robot_rover <763@Gmail>
2019-10-03 14:44:19 -0500

    Benchmark by Comparing Backends

--- benches/backend.rs
+++ benches/backend.rs
@@ -3,13 +3,15 @@ extern crate criterion;
 extern crate bsdiff_rs;
 extern crate rand_pcg;
 
-use criterion::black_box;
+use criterion::{black_box, BenchmarkId};
 use criterion::Criterion;
 
-use bsdiff_rs::raw::{bsdiff_raw, bspatch_raw};
 use rand::Rng;
 use std::io;
 use std::io::Write;
+use bsdiff_rs::{BsDiff, Backend};
+use bsdiff_rs::c::CBackend;
+use bsdiff_rs::rust::RustBackend;
 
 struct WriteDummy;
 
@@ -36,15 +38,28 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         .take(10_000)
         .collect();
     let mut write_dummy = WriteDummy {};
-    c.bench_function("bsdiff", |b| {
-        b.iter(|| bsdiff_raw(&test_data_1[..], &test_data_2[..], &mut write_dummy))
+
+    let mut group = c.benchmark_group("BsDiff");
+    group.bench_function(BenchmarkId::new("C Backend", ""), |b| {
+        b.iter(|| BsDiff::<CBackend>::bsdiff_raw(&test_data_1[..2_000], &test_data_2[..2_000], &mut write_dummy));
     });
+    group.bench_function(BenchmarkId::new("Rust Backend", ""), |b| {
+        b.iter(|| BsDiff::<RustBackend>::bsdiff_raw(&test_data_1[..2_000], &test_data_2[..2_000], &mut write_dummy));
+    });
+    group.finish();
+
     let mut patch_1 = Vec::new();
-    bsdiff_raw(&test_data_1[..], &test_data_2[..], &mut patch_1).unwrap();
+    BsDiff::<CBackend>::bsdiff_raw(&test_data_1[..], &test_data_2[..], &mut patch_1).unwrap();
     let testout = &mut [0u8; 10_000];
-    c.bench_function("bspatch", |b| {
-        b.iter(|| bspatch_raw(&test_data_1[..], testout, &mut &patch_1[..]))
+
+    let mut group = c.benchmark_group("BsPatch");
+    group.bench_function(BenchmarkId::new("C Backend", ""), |b| {
+        b.iter(|| BsDiff::<CBackend>::bspatch_raw(&test_data_1[..], testout, &mut &patch_1[..]))
     });
+    group.bench_function(BenchmarkId::new("Rust Backend", ""), |b| {
+        b.iter(|| BsDiff::<RustBackend>::bspatch_raw(&test_data_1[..], testout, &mut &patch_1[..]))
+    });
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
